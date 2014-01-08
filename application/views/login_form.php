9aa208f29d1f151c5948705e638fd03cb926da36
Femarie Kudo <femariejoymanga@gmail.com>
2014-01-08 14:49:24 +0800

    modify Login form

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,74 +1,77 @@
-    <link href="<?php echo base_url();?>css/bootstrap.css" rel="stylesheet" media="screen">
-    <div class = "navbar navbar-inverse">
-		<div class = "navbar-inner">
-			<a class = "brand" href = "D:/bootstrap/reg1.html">ICDRRIS</a>
-			<ul class = "nav">
-				<li class = "active"><a href = "D:/bootstrap/reg1.html">Home</a></li>
-			</ul>
-		
-			<ul class="nav pull-right">
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-						Log-in
-						<b class="caret"></b>
-					</a>
-					<ul class="dropdown-menu">
-						<form method = "post" action = "Login/validate_credentials" style = "margin:10px">
-						<?php 
-						$username_property = array( 'type' => 'text', 'name' => 'username', 'placeholder' => 'Username');
+  
+<?php if(!$this->session->userdata('is_logged_in')){ ?>
+	<ul class="nav pull-right">
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+				Log-in
+				<b class="caret"></b>
+			</a>
+			<ul class="dropdown-menu">
+									
+				<div class="error alert-error" style= "margin: .5em .5em .5em .5em">
+					<a class="close" data-dismiss="alert">×</a> 
+					<div style= "padding:.5em"><strong>Error!</strong>	<br />
+						<font size= "2"><?php print validation_errors(); ?></font>
+					</div>
+				</div>
+									
+				<form method = "post" action = "<?php echo base_url();?>Login/validate_credentials" style = "margin:10px">
+					<?php 
+					$username_property = array( 'type' => 'text', 'name' => 'username', 'placeholder' => 'Username','value'=>set_value('username'));
 						echo form_input($username_property); 
-						$password_property = array( 'type' => 'password', 'name' => 'password', 'placeholder' => 'Password');
+					$password_property = array( 'type' => 'password', 'name' => 'password', 'placeholder' => 'Password');
 						echo form_input($password_property); 
-						$submit_property = array( 'type' => 'submit', 'class' => 'btn btn-primary', 'style' => 'width:100%', 'name' => 'login', 'value' =>'Login');
+					$submit_property = array( 'type' => 'submit', 'class' => 'btn btn-primary', 'style' => 'width:100%', 'name' => 'login', 'value' =>'Login');
 						echo form_submit($submit_property);
-						?>
-										
-						</form>
-						<li class = "divider"></li>
-						<p style = "margin:10px">Not a member? <a href = "/icdrris/Login/signup">Signup now!</a></p>
-					</ul>
-				</li>
+					?>
+								
+				</form>
+				<li class = "divider"></li>
+				<p style = "margin:10px">Not a member? <a href = "<?php echo base_url();?>Login/signup">Signup now!</a></p>
 			</ul>
-			
-			<form class = "navbar-search pull-right">
-				<input type = "text" class = "search-query" placeholder = "Search">
-			</form>
-			
-			
-			
-		</div>
-	</div>
+		</li>
+	</ul>
+<?php } ?>	
 
-	
-	<div class = "row-fluid">
+ <!-- SEARCH BAR FROM HEADER
+		<form class = "navbar-search pull-right">
+			<input type = "text" class = "search-query" placeholder = "Search">
+		</form>	
+	</div>
+</div>
+-->
+
+<!--	CALL VIEW 'SIGNUP' INSTEAD	
+<div class = "row-fluid">
 	<div class = "span3"></div>
 	<div class = "span6">
-	
-	<h1>Sign up.</h1>
-	<br>
-	  
-	
-	<fieldset>
-		<?php echo form_open('Login/create_account');?>
-		<div class = "row-fluid">
-		<strong>First Name</strong><br>
-		<input type = "text" class = "span8"/></div>
-		<div class = "row-fluid">
-		<strong>Last Name</strong><br>
-		<input type = "text" class = "span8"/></div>
-		<div class = "row-fluid">
-		<strong>Username</strong><br>
-		<input type = "text" class = "span8"/></div>
-		<div class = "row-fluid">
-		<strong>Password</strong><br>
-		<input type = "password" class = "span8"></div>
-		<div class = "row-fluid">
-		<input type = "submit" class = "btn btn-primary" name = "login" value = "Sign up"/>
-		</div>
+		<h1>Sign up.</h1>
+		<br>
+
+		<fieldset>
+			<?php //echo form_open('Login/create_account');?>
+			<div class = "row-fluid">
+				<strong>First Name</strong><br>
+				<input type = "text" class = "span8"/>
+			</div>
+			<div class = "row-fluid">
+				<strong>Last Name</strong><br>
+				<input type = "text" class = "span8"/>
+			</div>
+			<div class = "row-fluid">
+				<strong>Username</strong><br>
+				<input type = "text" class = "span8"/>
+			</div>
+			<div class = "row-fluid">
+				<strong>Password</strong><br>
+				<input type = "password" class = "span8">
+			</div>
+			<div class = "row-fluid">
+				<input type = "submit" class = "btn btn-primary" name = "login" value = "Sign up"/>
+			</div>
 		</fieldset>
-		
+	
 	</div>
 	<div class = "span3"></div>
-	</div>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+</div> 
+-->
