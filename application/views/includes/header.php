089584fb6aeef62a6c6ff672414024b57bd55fff
teamdasig-james <jasencion@visp.net>
2014-01-12 22:49:20 +1100

    Merge origin/master
    Conflicts:
    	application/views/polyHome.php

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -12,9 +12,10 @@
 	   <!--    <link href="<?php //echo base_url();?>css/bootstrap-glyphicons.css" rel="stylesheet">-->
 	   
 		<script type="text/javascript" src="<?php echo base_url();?>js/jquery-1.3.2.js"></script>
-		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+                <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 		<script src="application/views/js/map.js"></script>
-
+               
 		<style type = "text/css">
 			#googlemap img,object,embed{max-width:none}
 			#map_canvas embed{max-width:none}
