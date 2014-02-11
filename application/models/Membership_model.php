519b2ab4cae23f74178a4125e9c0080537c15eab
Femarie Kudo <femariejoymanga@gmail.com>
2014-02-11 19:46:24 +0800

    Signup

--- application/models/Membership_model.php
+++ application/models/Membership_model.php
@@ -2,9 +2,9 @@
 
 class Membership_model extends CI_Model
 {
-        function getUserDetails($adminId){
+        function getUserDetails($user_name){
                 
-		$query = $this->db->query('SELECT u.adminId, u.fname, u.lname, u.utype FROM icdrris.users u WHERE u.adminId= "'.$adminId.'"');
+		$query = $this->db->query('SELECT u.user_id, u.user_name, u.user_first_name, u.user_last_name, u.user_type FROM icdrris.users u WHERE u.user_name= "'.$user_name.'"');
                 
                 if($query){
                    return $query; 
@@ -19,9 +19,9 @@ class Membership_model extends CI_Model
        //       $username=$this->input->post('username');
        //	$password=$this->input->post('password');
 
-                $this->db->select('adminId', 'password', 'fname', 'lname', 'utype');
+		$this->db->select('user_name', 'password', 'user_first_name', 'user_last_name', 'user_type');
 		$this->db->from('users');
-		$this->db->where('adminId', $username);
+		$this->db->where('user_name', $username);
 		$this->db->where('password', $password);
 	
 		$query = $this->db->get();
@@ -31,15 +31,16 @@ class Membership_model extends CI_Model
 			return true;
 		}
 	}
-	function create_account($username, $password, $fname, $lname, $utype)
+	function create_account($username, $password, $fname, $lname, $utype, $user_email)
 	{
 
 		$data= array(
-                            'adminId' => $username, 
+                            'user_name' => $username, 
                             'password' => $password,
-                            'fname' => $fname,
-                            'lname' => $lname,
-                            'utype' => $utype);
+                            'user_first_name' => $fname,
+                            'user_last_name' => $lname,
+                            'user_type' => $utype,
+                            'user_email' => $user_email);
 		
 		$query= $this->db->insert('users', $data);
                 if($query){
