a43f9172e4af078879a2abfd330641ec42e04f78
Femarie Kudo <femariejoymanga@gmail.com>
2014-02-20 15:28:36 +0800

    Rate Victim Show Details Edit victim Report Victim

--- application/views/js/formSubmission.js
+++ application/views/js/formSubmission.js
@@ -123,10 +123,10 @@ function editVictim(element){
 			var lastname  = $(this).data('lastname');
 			var address  = $(this).data('address');
 			var victimstatus  = $(this).data('victimstatus');
-			$("#first_name").attr("value", firstname);
-			$("#middle_name").attr("value", middlename);
-			$("#last_name").attr("value", lastname);
-			$("#address").attr("value", address);
+			$("#modalUpdateVictim #first_name").attr("value", firstname);
+			$("#modalUpdateVictim #middle_name").attr("value", middlename);
+			$("#modalUpdateVictim #last_name").attr("value", lastname);
+			$("#modalUpdateVictim #address").attr("value", address);
 			//$("#victim_status").attr("value", victimstatus);
 			var i=0;
 			while ((document.updateVictimForm.victim_status.options[i].value != victimstatus) && (i < document.updateVictimForm.victim_status.options.length))
@@ -143,10 +143,10 @@ function editVictim(element){
         // Get some values from elements on the modal login page:
         var incidentid  = $('#modalUpdateVictim').data('incidentid');
 		var victimid  = $('#modalUpdateVictim').data('victimid');
-        var firstname = $("#first_name").val();
-        var middlename = $("#middle_name").val();
-        var lastname = $("#last_name").val();
-        var address = $("#address").val();
+        var firstname = $("#modalUpdateVictim #first_name").val();
+        var middlename = $("#modalUpdateVictim #middle_name").val();
+        var lastname = $("#modalUpdateVictim #last_name").val();
+        var address = $("#modalUpdateVictim #address").val();
         var victimstatus = document.updateVictimForm.victim_status.value;
         var dataStr = 'incidentid='+incidentid+'&victimid='+victimid+'&firstname='+firstname+'&middlename='+middlename+'&lastname='+lastname+'&address='+address+'&victimstatus='+victimstatus;
 		console.log(dataStr);
@@ -175,61 +175,53 @@ function editVictim(element){
 	
 };
 
-//UPDATE INCIDENT
-	$('#editinfo-li').on('click', function(e){
+//REPORT VICTIM
+$(document).ready(function(){
+	$('#reportvictim-li').on('click', function(e){
 		e.preventDefault();
-		console.log('edit-incident clicked');
+		console.log('report-victim clicked');
 		
 		var incidentid = $(this).data('incidentid');
-		console.log('update incident: '+ incidentid);
-		$('#modalUpdateIncident').data('incidentid',incidentid);	//for AND query
+		console.log('report victim-incidentid: '+ incidentid);
+		$('#modalReportVictim').data('incidentid',incidentid);	//for AND query
 		
-		$('#modalUpdateIncident').modal('show');
+		$('#modalReportVictim').modal('show');
 	});
 	
-	$('#modalUpdateIncident').on('show.bs.modal', function(){
+	$('#modalReportVictim').on('show.bs.modal', function(){
 			var incidentid  = $(this).data('incidentid');
-			$("#first_name").attr("value", firstname);
-			$("#middle_name").attr("value", middlename);
-			$("#last_name").attr("value", lastname);
-			$("#address").attr("value", address);
-			//$("#victim_status").attr("value", victimstatus);
-			var i=0;
-			while ((document.updateVictimForm.victim_status.options[i].value != victimstatus) && (i < document.updateVictimForm.victim_status.options.length))
-			  {i++;}
-			if (i < document.updateVictimForm.victim_status.options.length)
-			  {document.updateVictimForm.victim_status.selectedIndex = i;}
+			
 			removeBtn = $(this).find('.danger');
 	});
 	
-	    $('#updateVictimForm').submit(function(event){
+		$('#reportVictimForm').submit(function(event){
         // Stop form from submitting normally
         event.preventDefault();
         
         // Get some values from elements on the modal login page:
-        var incidentid  = $('#modalUpdateVictim').data('incidentid');
-		var victimid  = $('#modalUpdateVictim').data('victimid');
+        var incidentid  = $('#modalReportVictim').data('incidentid');
         var firstname = $("#first_name").val();
         var middlename = $("#middle_name").val();
         var lastname = $("#last_name").val();
         var address = $("#address").val();
-        var victimstatus = document.updateVictimForm.victim_status.value;
-        var dataStr = 'incidentid='+incidentid+'&victimid='+victimid+'&firstname='+firstname+'&middlename='+middlename+'&lastname='+lastname+'&address='+address+'&victimstatus='+victimstatus;
+        var victimstatus = document.reportVictimForm.victim_status.value;
+		console.log(victimstatus);
+        var dataStr = 'reportNo='+incidentid+'&first_name='+firstname+'&middle_name='+middlename+'&last_name='+lastname+'&address='+address+'&victim_status='+victimstatus;
 		console.log(dataStr);
         /* Send the data using post and put results to the members table */
 		request = $.ajax({
-			url: "http://localhost/icdrris/Victim/updateVictim",
+			url: "http://localhost/icdrris/Victim/validate",
 			type: "POST",
 			data: dataStr,
 			success: function(msg){
 				console.log("success");
 				console.log(msg);
 				if(msg == 'success'){
-					console.log('naedit na bai. check the database');
-					$('#modalUpdateVictim').modal('hide');
+					console.log('naadd na bai. check the database');
+					$('#modalReportVictim').modal('hide');
 				}else{
 					console.log('naay mali sa controller or model. recheck the code.')
-					$(".modal-body").innerHTML(msg);
+					$("#modalReportVictim.modal-body").html(msg);
 				}
 			},
 			error: function(){
@@ -239,3 +231,42 @@ function editVictim(element){
 		});
     });
 	
+});
+	
+function detailsVictim(element){
+	$('.details-victim').on('click', function(e){
+		e.preventDefault();
+		console.log('details-victim clicked');
+		
+		var incidentid = $(this).data('incidentid');
+		var victimid = $(this).data('victimid');
+		var firstname = $(this).data('firstname');
+		var middlename = $(this).data('middlename');
+		var lastname = $(this).data('lastname');
+		var address = $(this).data('address');
+		var victimstatus = $(this).data('victimstatus');
+		var report_rating_true = $(this).data('ratingtrue');
+		var report_rating_false = $(this).data('ratingfalse');
+		var flag_confirmed = $(this).data('flagconfirmed');
+		console.log('show details victim: '+ incidentid + ' ' + victimid + ' '+ firstname+ ' '+ middlename+ ' '+ lastname+ ' '+ address+ ' '+ victimstatus+ ' '+ report_rating_true + ' ' + report_rating_false+ ' ' + flag_confirmed);
+		$( "#full_name" ).html( document.createTextNode( firstname + " "+ middlename+ " "+lastname) );
+		$( "#address" ).html( document.createTextNode( address) );
+		$( "#victim_status" ).html( document.createTextNode( victimstatus) );
+		$( "#modalDetailsVictim .rateTrue" ).html( document.createTextNode( report_rating_true) );
+		$( "#modalDetailsVictim .rateFalse" ).html( document.createTextNode( report_rating_false) );
+			
+			$( "#approved-victim, #disapproved-victim" ).attr( "data-victimid", victimid);
+			$( "#approved-victim, #disapproved-victim" ).attr( "data-incidentid", incidentid);
+		if(flag_confirmed == 0){
+			$( "#flag_confirmed" ).attr( "class", "span11 alert alert-error");
+			$( "#flag_confirmed" ).html( "The victim is still not confirmed.");
+		}
+		if(flag_confirmed == 1){
+			$( "#flag_confirmed").attr("class", "span11 alert alert-info");
+			$( "#flag_confirmed" ).html( "The victim was already confirmed.");
+		}
+		console.log('passed the data to the modalDetailsVictim');
+		$('#modalDetailsVictim').modal('show');
+	});
+
+};
