caa19cce2e5a646655e6d153804dd414dc7e6884
Czar Leigh <czarleigh@gmail.com>
2014-03-01 15:39:54 +0800

    dropdown for skills

--- application/views/dropdowntrial.php
+++ application/views/dropdowntrial.php
@@ -1,5 +1,4 @@
 </div></div>
-<div class = "container-fluid">
 <!--	<div class="multiselect">
     	<label><input type="checkbox" name="option[]" value="1" />Green</label>
     	<label><input type="checkbox" name="option[]" value="2" />Red</label>
@@ -50,13 +49,76 @@
 	});
 
 </script> -->
+<div class = "container-fluid">
+<div class="dropdown">
+    <a class="dropdown-toggle btn" data-toggle="dropdown" href="#">
+        Select Skills
+        <b class="caret"></b>
+    </a>
+    <ul class="dropdown-menu dropdown-menu-form" role="menu">
+       <?php 
+        foreach($skills as $skill) {
+          echo "<li><label class = \"checkbox\"><input type = \"checkbox\">".$skill->skillset_description."</label></li>";
+        }
+       ?>
 
-	<select name = "trial">
-		<option value = "null">Select</option>
-	<?php	foreach($skills as $skill) {
-			echo "<label class=\"checkbox\"><input type=\"checkbox\">";
-			echo "<option value = \"".$skill->skillset_description."\">".$skill->skillset_description."</option>";
-			echo "</label>";
-		} ?>
-	</select>
+   
+    </ul>
 </div>
+</div>
+
+<!--
+<div class = "container-fluid">
+  <div class = "row-fluid">
+    <form class = "form-horizontal">
+    <div class = "control-group"><label class = "control-label" for="name">Skills: </label><div class = "controls"><select multiple="multiple"><?php foreach($skills as $skill) { echo "<input type = \"checkbox\" data-label = \"suffix\" /><option id=".$skill->skillset_id.">".$skill->skillset_description."</option>";}?></select></div></div>
+  </form></div>
+</div>
+
+-->
+<!--
+
+<div class = "toppanel1">
+<div class="center"><p>View Incidents by:</p>   
+                          <a class="dropdown-toggle btn" data-toggle="dropdown" href="#">
+                              Select Elements
+                              <b class="caret"></b>
+                          </a>
+    <ul class = "dropdown-menu dropdown-menu-form" role="menu">
+
+	<?php	foreach($skills as $skill) {
+      echo "<li><label class = \"checkbox\"><input type=\"checkbox\">".$skill->skillset_description."</label></li>";
+		
+		} ?></ul>
+	</div>
+</div>
+-->
+
+<!--<div id="controls" style="display:block">
+            <div id="elementBoxes">
+            <div class="toppanel">
+                <form class = "navbar-form pull-left" name="filterForm1">
+                    <div class="center" align="center"><p>View Incidents by:</p>   
+                          <a class="dropdown-toggle btn" data-toggle="dropdown" href="#">
+                              Select Elements
+                              <b class="caret"></b>
+                          </a>
+                          <ul class="dropdown-menu dropdown-menu-form" role="menu">
+                              <li>
+                                  <label class="checkbox">
+                                      <input type="checkbox">
+                                      Marker
+                                  </label>
+                              </li>
+                              <li>
+                                  <label class="checkbox">
+                                      <input type="checkbox">
+                                      Polygon
+                                  </label>
+                              </li>                                                                                                                      
+                          </ul>
+                      </div> 
+                </form>
+            </div>
+            </div>
+        </div>-->
